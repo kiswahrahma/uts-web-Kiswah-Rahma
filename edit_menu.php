a6a4346a6067bab371e26b4ffedaa9509f25962c
Kiswah Rahma <izzatikiswahrahma@gmail.com>
2026-07-19 20:41:19 +0700

    update edit_menu

--- edit_menu.php
+++ edit_menu.php
@@ -97,6 +97,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Menu - Cafe Kiswah</title>
     <link rel="stylesheet" href="css/style.css">
+    <script src="js/dark-mode.js" defer></script>
 </head>
 <body>
 
