a5dffa2b0f3c8d480cbc4854d42836d48658789e
Kiswah Rahma <izzatikiswahrahma@gmail.com>
2026-07-19 20:51:56 +0700

    update lupa password

--- lupa_password.php
+++ lupa_password.php
@@ -108,6 +108,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lupa Password - Cafe Kiswah</title>
     <link rel="stylesheet" href="css/style.css">
+    <script src="js/dark-mode.js" defer></script>
 </head>
 <body class="halaman-auth">
 
